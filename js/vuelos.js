b6a1569e8f6353b3dd07f80b1d122b5fd7bf2e90
Alan Ibañez <alanleonelib@gmail.com>
2026-06-13 15:54:04 -0300

    Se sigue trabajando en los vuelos

---
+++ js/vuelos.js
@@ -0,0 +1,72 @@
+
+const vuelos = [ezeiza_madrid, ezeiza_mendoza_aa, ezeiza_mendoza_la, ezeiza_cancun, ezeiza_miami, ezeiza_camboriu, ezeiza_san_juan]
+
+const ezeiza_madrid = {
+    origen: "EZE", 
+    destino: "MAD", 
+    salida: "06/05/2024 – 23:55 - 16:15", 
+    llegada: "10/05/2024 – 17:45 - 23:40", 
+    escala: "1 escala LIM (ida y vuelta)", 
+    precio: 651,
+    aerolinea: "Fly Emirates",
+};
+
+const ezeiza_mendoza_aa = {
+    origen: "EZE", 
+    destino: "MDZ", 
+    salida: "06/05/2026 – 08:30 - 10:30", 
+    llegada: "10/05/2024 – 17:45 - 23:40", 
+    escala: "Sin escala", 
+    precio: 75000,
+    aerolinea: "Aerolineas Argentinas",
+};
+
+const ezeiza_mendoza_la = {
+    origen: "EZE", 
+    destino: "MDZ", 
+    salida: "06/05/2026 – 08:30 - 10:30", 
+    llegada: "10/05/2024 – 17:45 - 23:40", 
+    escala: "Sin escala", 
+    precio: 90000,
+    aerolinea: "LATAM Airlines",
+};
+
+const ezeiza_cancun = {
+    origen: "EZE", 
+    destino: "CUN", 
+    salida: "06/05/2026 – 06:40 - 15:00", 
+    llegada: "10/05/2024 – 17:45 - 23:40", 
+    escala: "Sin escala", 
+    precio: 900,
+    aerolinea: "Aerolineas Argentinas",
+};
+
+const ezeiza_miami = {
+    origen: "EZE", 
+    destino: "MIA", 
+    salida: "06/05/2026 – 14:15 - 00:25", 
+    llegada: "10/05/2024 – 17:45 - 23:40", 
+    escala: "Sin escala", 
+    precio: 750,
+    aerolinea: "LATAM Airlines",
+};
+
+const ezeiza_camboriu = {
+    origen: "EZE", 
+    destino: "CAM", 
+    salida: "06/05/2026 – 07:20 - 09:30", 
+    llegada: "10/05/2024 – 17:45 - 23:40", 
+    escala: "Sin escala", 
+    precio: 500,
+    aerolinea: "Aerolineas Argentinas",
+};
+
+const ezeiza_san_juan = {
+    origen: "EZE", 
+    destino: "SJN", 
+    salida: "06/05/2026 – 05:50 - 16:30", 
+    llegada: "10/05/2024 – 17:45 - 23:40", 
+    escala: "Sin escala", 
+    precio: 950,
+    aerolinea: "Aerolineas Argentinas",
+};

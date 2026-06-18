2442ad258e4126f3ad2f16028d30efcf5b3c9ab8
Alan Ibañez <alanleonelib@gmail.com>
2026-06-18 00:48:10 -0300

    Se soluciona los filtros

--- js/resultados.js
+++ js/resultados.js
@@ -1,31 +1,11 @@
- document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
     // FILTRO SEGÚN BÚSQUEDA DEL HOME
     const destinoBuscado = localStorage.getItem("destino");
 
     console.log("Destino buscado:", destinoBuscado);
 
-    if (destinoBuscado) {
-
-        const vuelosFiltrar = document.querySelectorAll(".vuelo-card");
-
-        vuelosFiltrar.forEach(function (vuelo) {
-
-            const ciudades = vuelo.querySelectorAll(".ciudad");
-            const ciudadDestino = ciudades[1].textContent.trim();
-
-            console.log("Vuelo:", ciudadDestino);
-
-            if (
-                ciudadDestino.toLowerCase() !==
-                destinoBuscado.toLowerCase()
-            ) {
-                vuelo.style.display = "none";
-            }
-
-        });
-
-    }
+    
 
     // BOTONES SELECCIONAR
     const botones = document.querySelectorAll(".btn-select");
@@ -36,25 +16,25 @@
             const equipaje = Number(equipajeSeleccionado);
             const totalbase = Number(this.dataset.total);
             const vuelo = {
-                     aerolinea: this.dataset.aerolinea,
-                     logo: this.dataset.logo,
-                     origen: this.dataset.origen,
-                    ciudadOrigen: this.dataset.ciudadOrigen,
-                    destino: this.dataset.destino,
-                     ciudadDestino: this.dataset.ciudadDestino,
-                     salida: this.dataset.salida,
-                   llegada: this.dataset.llegada,
-                   duracion: this.dataset.duracion,
-                 salidaVuelta: this.dataset.salidaVuelta,
-                  llegadaVuelta: this.dataset.llegadaVuelta,
-                   duracionVuelta: this.dataset.duracionVuelta,
-                  moneda: this.dataset.moneda,
-                  precioBase: this.dataset.precioBase,
-                   equipaje:equipaje,
-                  asientos: this.dataset.asientos,s
-                  impuestos: this.dataset.impuestos,
-                  total:totalbase + equipaje,
-                 pago: this.dataset.pago
+                aerolinea: this.dataset.aerolinea,
+                logo: this.dataset.logo,
+                origen: this.dataset.origen,
+                ciudadOrigen: this.dataset.ciudadOrigen,
+                destino: this.dataset.destino,
+                ciudadDestino: this.dataset.ciudadDestino,
+                salida: this.dataset.salida,
+                llegada: this.dataset.llegada,
+                duracion: this.dataset.duracion,
+                salidaVuelta: this.dataset.salidaVuelta,
+                llegadaVuelta: this.dataset.llegadaVuelta,
+                duracionVuelta: this.dataset.duracionVuelta,
+                moneda: this.dataset.moneda,
+                precioBase: this.dataset.precioBase,
+                equipaje: equipaje,
+                asientos: this.dataset.asientos,
+                impuestos: this.dataset.impuestos,
+                total: totalbase + equipaje,
+                pago: this.dataset.pago
             };
 
             localStorage.setItem(
@@ -80,23 +60,16 @@
     const rangePrecio = document.getElementById("filtroPrecio");
     const valorPrecio = document.getElementById("valorPrecio");
 
+    aplicarFiltros();
+
     if (rangePrecio) {
 
         rangePrecio.addEventListener("input", function () {
 
-            valorPrecio.textContent = this.value;
+        
 
-            vuelos.forEach(function (vuelo) {
-
-                const precio = Number(vuelo.dataset.precio);
-
-                if (precio <= Number(rangePrecio.value)) {
-                    vuelo.style.display = "block";
-                } else {
-                    vuelo.style.display = "none";
-                }
-
-            });
+         valorPrecio.textContent = this.value;
+        aplicarFiltros();
 
         });
 
@@ -110,37 +83,7 @@
 
         checkbox.addEventListener("change", function () {
 
-            vuelos.forEach(function (vuelo) {
-
-                const aerolinea =
-                    vuelo.querySelector(".aerolinea-option span")
-                        .textContent;
-
-                let mostrar = false;
-
-                filtroAerolineas.forEach(function (check) {
-
-                    if (
-                        check.checked &&
-                        aerolinea.includes(check.value)
-                    ) {
-                        mostrar = true;
-                    }
-
-                });
-
-                if (
-                    document.querySelectorAll(
-                        ".filtroAerolinea:checked"
-                    ).length === 0
-                ) {
-                    mostrar = true;
-                }
-
-                vuelo.style.display =
-                    mostrar ? "block" : "none";
-
-            });
+          aplicarFiltros();
 
         });
 
@@ -150,28 +93,28 @@
     let equipajeSeleccionado = 0;
     const equipajes = document.querySelectorAll(".equipaje-card");
 
-equipajes.forEach(function (equipaje) {
-  equipaje.addEventListener("click", function () {
+    equipajes.forEach(function (equipaje) {
+        equipaje.addEventListener("click", function () {
 
-    equipajes.forEach(function (e) {
-      e.classList.remove("seleccionado");
+            equipajes.forEach(function (e) {
+                e.classList.remove("seleccionado");
+            });
+
+            this.classList.add("seleccionado");
+
+            const texto = this.querySelector("span").textContent;
+
+            if (texto.includes("45")) {
+                equipajeSeleccionado = 45;
+            } else if (texto.includes("85")) {
+                equipajeSeleccionado = 85;
+            } else {
+                equipajeSeleccionado = 0;
+            }
+
+            console.log("Equipaje seleccionado:", equipajeSeleccionado);
+        });
     });
-
-    this.classList.add("seleccionado");
-
-    const texto = this.querySelector("span").textContent;
-
-    if (texto.includes("45")) {
-      equipajeSeleccionado = 45;
-    } else if (texto.includes("85")) {
-      equipajeSeleccionado = 85;
-    } else {
-      equipajeSeleccionado = 0;
-    }
-
-    console.log("Equipaje seleccionado:", equipajeSeleccionado);
-  });
-});
 
     // ORDENAR VUELOS
     const selectOrdenar =
@@ -184,7 +127,7 @@ equipajes.forEach(function (equipaje) {
             function () {
 
                 const contenedor =
-                    document.querySelector(".resultados");
+                    document.querySelector(".lista-vuelos");
 
                 const vuelosArray = Array.from(
                     document.querySelectorAll(".vuelo-card")
@@ -235,6 +178,64 @@ equipajes.forEach(function (equipaje) {
         );
 
     }
+
+    function aplicarFiltros() {
+
+        console.log("Aplicando filtros");
+
+    const precioMax = Number(rangePrecio.value);
+
+    const aerolineasSeleccionadas = Array.from(
+        document.querySelectorAll(".filtroAerolinea:checked")
+    ).map(check => check.value);
+
+    vuelos.forEach(function (vuelo) {
+
+        // FILTRO DESTINO
+        let coincideDestino = true;
+
+        if (destinoBuscado) {
+
+            const ciudades = vuelo.querySelectorAll(".ciudad");
+            const ciudadDestino = ciudades[1].textContent.trim();
+
+            coincideDestino =
+                ciudadDestino.toLowerCase() ===
+                destinoBuscado.toLowerCase();
+        }
+
+        // FILTRO PRECIO
+        const coincidePrecio =
+            Number(vuelo.dataset.precio) <= precioMax;
+
+        // FILTRO AEROLÍNEA
+        let coincideAerolinea = true;
+
+        if (aerolineasSeleccionadas.length > 0) {
+
+            const aerolinea =
+                vuelo.querySelector(".aerolinea-option span")
+                    .textContent;
+
+            coincideAerolinea =
+                aerolineasSeleccionadas.some(nombre =>
+                    aerolinea.includes(nombre)
+                );
+        }
+
+        // MOSTRAR U OCULTAR
+        if (
+            coincideDestino &&
+            coincidePrecio &&
+            coincideAerolinea
+        ) {
+            vuelo.style.display = "block";
+        } else {
+            vuelo.style.display = "none";
+        }
+
+    });
+}
 
 });
 

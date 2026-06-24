b2b8894d8de7434c13edf0e09c8ec2a19ead6821
Alan Ibañez <alanleonelib@gmail.com>
2026-06-23 22:40:40 -0300

    Modal hecho

--- js/home.js
+++ js/home.js
@@ -47,25 +47,43 @@ formulario.addEventListener("submit", function(event){
 });
 
 
+const modal = document.querySelector(".modal_personas");
+const btnContinuar = document.querySelector(".btn_modal_continuar");
+const selectPersonasModal = document.querySelector("#cantidad_personas");
+
 const ofertasHome = document.querySelectorAll(".caja-oferta");
 
 ofertasHome.forEach(function (oferta) {
+
     oferta.addEventListener("click", function (e) {
+
         e.preventDefault();
 
-       
-        const destinoOferta = this.dataset.destino;
-        const idaOferta = this.dataset.fechaIda;
-        const vueltaOferta = this.dataset.fechaVuelta;
-
-        
         localStorage.setItem("origen", "Buenos Aires");
-        localStorage.setItem("destino", destinoOferta);
-        localStorage.setItem("fechaIda", idaOferta);
-        localStorage.setItem("fechaVuelta", vueltaOferta);
-        localStorage.setItem("personas", "1"); 
+        localStorage.setItem("destino", this.dataset.destino);
+        localStorage.setItem("fechaIda", this.dataset.fechaIda);
+        localStorage.setItem("fechaVuelta", this.dataset.fechaVuelta);
 
-        
-        window.location.href = "pages/resultados.html";
+        modal.style.display = "flex";
+
     });
+
+});
+
+btnContinuar.addEventListener("click", function () {
+
+    const cantidadPersonas = selectPersonasModal.value;
+
+    localStorage.setItem("personas", cantidadPersonas);
+
+    window.location.href = "pages/resultados.html";
+
+});
+
+modal.addEventListener("click", function (e) {
+
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+
 });
--- js/ofertas.js
+++ js/ofertas.js
@@ -1,28 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const botonesOferta = document.querySelectorAll(".btn-oferta");
+    const modal = document.querySelector(".modal_personas");
+    const btnContinuar = document.querySelector(".btn_modal_continuar");
+    const selectPersonas = document.querySelector("#cantidad_personas");
 
     botonesOferta.forEach(function (boton) {
 
         boton.addEventListener("click", function (e) {
-            
-            e.preventDefault(); 
 
-            
-            const destinoOferta = this.dataset.destino;
-            const idaOferta = this.dataset.fechaIda;
-            const vueltaOferta = this.dataset.fechaVuelta;
+            e.preventDefault();
 
-            localStorage.setItem("origen", "Buenos Aires"); 
-            localStorage.setItem("destino", destinoOferta);
-            localStorage.setItem("fechaIda", idaOferta);
-            localStorage.setItem("fechaVuelta", vueltaOferta);
-            localStorage.setItem("personas", "1"); 
+            localStorage.setItem("origen", "Buenos Aires");
+            localStorage.setItem("destino", this.dataset.destino);
+            localStorage.setItem("fechaIda", this.dataset.fechaIda);
+            localStorage.setItem("fechaVuelta", this.dataset.fechaVuelta);
 
-            console.log("Oferta seleccionada:", destinoOferta, "del", idaOferta, "al", vueltaOferta);
+            // Mostrar modal
+            modal.style.display = "flex";
 
-            window.location.href = "./resultados.html";
         });
+
+    });
+
+    btnContinuar.addEventListener("click", function () {
+
+        const cantidadPersonas = selectPersonas.value;
+
+        localStorage.setItem("personas", cantidadPersonas);
+
+        window.location.href = "./resultados.html";
+
+    });
+
+    // Cerrar modal al hacer click en el fondo oscuro
+    modal.addEventListener("click", function (e) {
+
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
 
     });
 
